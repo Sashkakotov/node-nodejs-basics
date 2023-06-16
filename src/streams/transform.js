9a2e222fcdc3b9b0325a3a702f845e8d89dce6ca
Sashkakotov <sashkakotov@mail.ru>
2023-06-16 18:49:13 +0300

    feat: streams

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,5 @@
 const transform = async () => {
-    // Write your code here 
+  stdin.on("data", (data) => stdout.write(data.slice(0, -1).reverse() + "\n"));
 };
 
 await transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import { createWriteStream } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const write = async () => {
-    // Write your code here 
+  const file = path.join(__dirname, "files", "fileToWrite.txt");
+  const writebleStream = createWriteStream(file, { flags: "a" });
+  process.stdin.pipe(writebleStream);
 };
 
 await write();
