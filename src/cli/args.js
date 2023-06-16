550cd5e5c7614fc8d4eb0e4e8284d84cc6c8cb8e
Sashkakotov <sashkakotov@mail.ru>
2023-06-16 16:08:41 +0300

    feat: cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+    const values = [];
+    process.argv.forEach((value, i) => {
+        if(value.slice(0, 2) === '--') {
+            values.push(`${value.slice(2)} is ${process.argv[i+1]}`)
+        }
+    });
+    console.log(values.join(', '))
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -2,10 +2,10 @@ const parseEnv = () => {
   const values = [];
   for (let value of Object.keys(process.env)) {
     if (value.slice(0, 4) === "RSS_") {
-      value.push(`${val}=${process.env[val]}`);
+      values.push(`${value}=${process.env[value]}`);
     }
   }
-  console.log(vals.join("; "));
+  console.log(values.join("; "));
 };
 
 parseEnv();
