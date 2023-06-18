135a493e96f754d66fe84d0525041401fa67b5f7
Sashkakotov <sashkakotov@mail.ru>
2023-06-18 11:44:27 +0300

    feat: wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,27 @@
+import path from "path";
+import { Worker } from "worker_threads";
+
+import { fileURLToPath } from "url";
+import { cpus } from "os";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const CPU = cpus().length;
+const worker = path.join(__dirname, "worker.js");
+
 const performCalculations = async () => {
-    // Write your code here
+  const result = [];
+  for (let i = 0; i < CPU; i++) {
+    const workRes = await new Promise((resolve) => {
+      const currentWorker = new Worker(worker, { workerData: 10 + i });
+      currentWorker.on("message", (msg) =>
+        resolve({ status: "resolved", data: msg })
+      );
+      currentWorker.on("error", () => resolve({ status: "error", data: null }));
+      currentWorker.on("exit", () => resolve({ status: "error", data: null }));
+    });
+    result.push(workRes);
+  }
+  console.log(result);
 };
 
 await performCalculations();
