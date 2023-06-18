4128d5d185782414f731d6a5f0d8bdad0ef78655
Sashkakotov <sashkakotov@mail.ru>
2023-06-18 11:58:37 +0300

    feat: cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,25 @@
+import path from "path";
+import cp from "child_process";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const file = path.join(__dirname, "files", "script.js");
+  
+  const node = cp.spawn(`node`, [file].concat(args));
+
+  node.on("data", (data) => process.stdout.write(data));
+  node.on("error", (err) => {
+    console.log(`error: ${err.message}`);
+  });
+  node.on("close", (code) => {
+    console.log(`childe process closed with code: ${code}`);
+    process.exit();
+  });
+
+  node.stdout.pipe(process.stdin);
+  process.stdout.pipe(node.stdin);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["someArgument1", "someArgument2", "someArgument3"]);
--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,16 @@
+import fs from "fs";
+import path from "path";
+import zlib from "zlib";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const compress = async () => {
-    // Write your code here 
+  const file = path.join(__dirname, "files", "fileToCompress.txt");
+  const archiveFile = path.join(__dirname, "files", "archive.gz");
+  const readableStream = fs.createReadStream(file);
+  const writableStream = fs.createWriteStream(archiveFile);
+  readableStream.pipe(zlib.createGzip()).pipe(writableStream);
 };
 
 await compress();
